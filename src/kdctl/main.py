98b6bd6cfe9132c178c5892a2f6b7335945e94bb
Byomakesh Maharana <byomakeshmaharana@gmail.com>
2025-07-08 01:25:36 +0530

    [feat] implement get deployment functionality

--- src/kdctl/main.py
+++ src/kdctl/main.py
@@ -10,9 +10,13 @@ from kubernetes import client as kclient
 from kubernetes import config as kconfig
 from kubernetes.client.rest import ApiException
 from typing_extensions import Annotated
+from rich.table import Table
+from rich.console import Console
 
 app = typer.Typer()
 logger = logging.getLogger(__name__)
+
+console = Console()
 
 def default_kube_config():
     """
@@ -25,19 +29,109 @@ def default_kube_config():
     
     return kube_config
 
+def pretty_display(info: list[dict]):
+    """
+    Displays Deployment info in a tabular form
+    """
+
+    table = Table(
+        title="Deployment status summary",
+        show_header=True,
+        header_style="bold bright_cyan",
+        border_style="dim"
+    )
+
+    table.add_column("Team", style="dim")
+    table.add_column("App name", style="cyan")
+    table.add_column("Revision", justify="right", style="green")
+    table.add_column("Created", style="bold red" if not all else "bold blue")
+    table.add_column("Replicas", style="blue italic")
+    table.add_column("Ready", style="blue italic")
+    table.add_column("Available", style="blue italic")
+
+    for entry in info:
+        table.add_row(
+            entry['team'],
+            entry['name'],
+            entry['revision'],
+            entry['created'],
+            str(entry['replicas']),
+            str(entry['ready']),
+            str(entry['available'])
+        )
+
+    console.print(table)
+    return None
+
 @app.command("get")
 def get_deployment(
+    team: Annotated[
+        str,
+        typer.Argument(
+            help="Specify team name"
+        ),
+    ],
     app: Annotated[
         str,
         typer.Argument(
             help="Specify app name"
         ),
     ],
+    kubeconfig: Annotated[
+        Path,
+        typer.Option(
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            writable=False,
+            readable=True,
+            resolve_path=True,
+            help="Path to the (Kubernetes) kubeconfig file",
+            default_factory=default_kube_config
+        )
+    ]
 ):
     """
     Get Deployment info using app name
     """
-    print(f"Placeholder: Return deployment details for {app} [WIP]")
+    try:
+        kube_config = yaml.safe_load(kubeconfig.read_bytes())
+        logger.debug("kubeconfig has valid yaml syntax, read successful ✓")
+    except yaml.YAMLError as e:
+        logger.critical(f"Failed to read config file. Error: {e}\nQuitting..")
+        sys.exit(1)
+
+    kconf = kclient.Configuration()
+    kconfig.load_kube_config_from_dict(
+        kube_config,
+        client_configuration=kconf
+    )
+    with kclient.ApiClient(kconf) as api_client:
+        api_instance = kclient.AppsV1Api(api_client)
+        name = app
+        namespace = team
+        pretty = 'false'
+
+    try:
+        api_response = api_instance.read_namespaced_deployment(
+            name,
+            namespace,
+            pretty=pretty
+        )
+        deployment_info = {}
+        deployment_info['team'] = api_response.metadata.labels['simplismart.ai/team-owner']
+        deployment_info['name'] = api_response.metadata.name
+        deployment_info['description'] = api_response.metadata.annotations['simplismart.ai/app-info']
+        deployment_info['revision'] = api_response.metadata.annotations['deployment.kubernetes.io/revision']
+        deployment_info['created'] = api_response.metadata.creation_timestamp.strftime("%b %d, %Y %H:%M")
+        deployment_info['replicas'] = api_response.status.replicas
+        deployment_info['ready'] = api_response.status.ready_replicas
+        deployment_info['available'] = api_response.status.available_replicas
+        info_batch = []
+        info_batch.append(deployment_info)
+        pretty_display(info_batch)
+    except ApiException as e:
+        print("Exception when calling AppsV1Api->read_namespaced_deployment: %s\n" % e)
 
     return None
 
